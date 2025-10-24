fcdeabfe913b73c0cbc614b5009dca83b70f8764
SebasAlRivas <seba_al89@hotmail.com>
2025-10-24 15:46:00 -0300

    feat: configuracion inicial de entorno (settings, urls) para conexion remota a BD

--- ISDMEstadodeCuenta/urls.py
+++ ISDMEstadodeCuenta/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include 
 
 urlpatterns = [
+    # 1. Ruta para el administrador de Django
     path('admin/', admin.site.urls),
-    path('app/', include('mi_app.urls')),
+    
+    # 2. Redirige la URL raíz ('/') a las URLs definidas en mi_app/urls.py
+    # ESTE ES EL PUNTO CLAVE QUE NO DEBE CAUSAR RECURSIÓN
+    path('', include('mi_app.urls')), 
 ]
