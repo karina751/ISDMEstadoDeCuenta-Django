5645b6f34d28a800b7178e6779aa8650c883d43d
Cristianosky88 <cristian_skater_6@hotmail.com>
2025-11-22 08:47:28 -0300

    Se agrego comentarios nuevos

--- backend/ISDMEstadodeCuenta/urls.py
+++ backend/ISDMEstadodeCuenta/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include 
-
+from django.contrib import admin # Importa el panel de administración de Django
+from django.urls import path, include # Importa funciones para definir rutas
+# Lista que contiene todas las URLs (direcciones) de tu sitio web
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('app/', include('mi_app.urls')),
+    path('admin/', admin.site.urls), # 1. Ruta para el panel de administrador
+    path('app/', include('mi_app.urls')), # 2. Ruta para conectar con tu aplicación "mi_app"
 ]
